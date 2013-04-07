cfc18951095a20906a5647efb61a674aa497dc7b
Tim Sharpe <tim@sharpe.id.au>
2013-04-07 10:49:55 -0700

    Fix so Minecraft.app will actually launch
    There's something different about the way that way we install Minecraft
    (extracting it directly to /Applications) vs the "standard" way (download to
    ~/Downloads, extract and copy to /Applications) that ends up with the Java
    launcher not getting execute bits set.
    
    re #1

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,4 +8,9 @@ class minecraft {
     source   => 'http://s3.amazonaws.com/MinecraftDownload/launcher/Minecraft.zip',
     provider => 'compressed_app'
   }
+
+  file { '/Applications/Minecraft.app/Contents/MacOS/JavaApplicationStub':
+    mode    => '0755',
+    require => Package['Minecraft'],
+  }
 }
--- spec/classes/minecraft_spec.rb
+++ spec/classes/minecraft_spec.rb
@@ -11,5 +11,10 @@ describe 'minecraft' do
       :source   => 'http://s3.amazonaws.com/MinecraftDownload/launcher/Minecraft.zip',
       :provider => 'compressed_app'
     })
+
+    should contain_file('/Applications/Minecraft.app/Contents/MacOS/JavaApplicationStub').with({
+      :mode    => '0755',
+      :require => 'Package[Minecraft]',
+    })
   end
 end
